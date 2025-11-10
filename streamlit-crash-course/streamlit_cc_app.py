23db786e7305b00e69cf45c6558b86c09138e6b7
lindsayalexandra14 <149544151+lindsayalexandra14@users.noreply.github.com>
2025-11-10 19:42:32 +0000

    test chart

--- streamlit-crash-course/streamlit_cc_app.py
+++ streamlit-crash-course/streamlit_cc_app.py
@@ -6,3 +6,12 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("divorce.csv")
+
+import matplotlib.pyplot as plt
+from numpy.random import default_rng as rng
+
+arr = rng(0).normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
