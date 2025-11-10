35db240848cb87220f7713fde4eca4363a9e438c
lindsayalexandra14 <149544151+lindsayalexandra14@users.noreply.github.com>
2025-11-10 19:48:36 +0000

    updates

--- streamlit-crash-course/streamlit_cc_app.py
+++ streamlit-crash-course/streamlit_cc_app.py
@@ -18,9 +18,6 @@ st.pyplot(fig)
 
 import plotly.express as px
 
-# Example data
-df = px.data.df()
-
 # Create figure with specific size
 fig = px.scatter(df, x="income_man", y="income_woman", color="num_kids")
 fig.update_layout(width=800, height=500)  # width & height in pixels
