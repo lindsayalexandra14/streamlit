327a300e738e32c5154379e8d4c5ed7a02519f6e
lindsayalexandra14 <149544151+lindsayalexandra14@users.noreply.github.com>
2025-11-14 06:46:27 +0000

    test

--- streamlit-crash-course/streamlit_cc_app.py
+++ streamlit-crash-course/streamlit_cc_app.py
@@ -256,7 +256,7 @@ def make_pairplot(
     # Update traces
     fig.update_traces(
         diagonal_visible=False,
-        marker=dict(size=5, opacity=0.7, color='black')
+        marker=dict(size=5, opacity=0.7, color='turquoise')
     )
 
     # Layout & margin
