5e856ddbda976a426970a514ce78466a64c5365f
lindsayalexandra14 <149544151+lindsayalexandra14@users.noreply.github.com>
2025-11-14 18:59:35 +0000

    test

--- streamlit-crash-course/streamlit_cc_app.py
+++ streamlit-crash-course/streamlit_cc_app.py
@@ -651,8 +651,8 @@ corrplot_caption = (
 st.markdown(corrplot_caption, unsafe_allow_html=True)
 
 chi_caption = (
-            "Chi-Squared test pairs the categorical variables to see if a significant relationship exists:<br><br>"
-            "(Note, number of kids has been used as numerical & categorical given its few unique values)<br>"
+            "Chi-Squared test pairs the categorical variables to see if a significant relationship exists:<br>"
+            "(Note, number of kids has been used as numerical & categorical given its few unique values)<br><br>"
             "• Highest signficance in relationship between Income (Man) & Income (Woman)<br>"
             "• All other relationships significant except for Education (Man) & Number of Kids"
         )
