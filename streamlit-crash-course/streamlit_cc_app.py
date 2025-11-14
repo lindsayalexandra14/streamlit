b0954362e89374ad3631193b28f125f6cfd7c5a7
lindsayalexandra14 <149544151+lindsayalexandra14@users.noreply.github.com>
2025-11-14 19:43:13 +0000

    test

--- streamlit-crash-course/streamlit_cc_app.py
+++ streamlit-crash-course/streamlit_cc_app.py
@@ -90,7 +90,8 @@ custom_palette = [
     '#1bc2bb'
 ]
 
-st.image("https://www.github.com/lindsayalexandra14/ds_portfolio/main/raw/1_projects/data_visualization/streamlit/divorce/rings.png", 
+st.image("https://raw.githubusercontent.com/lindsayalexandra14/ds_portfolio/main/1_projects/data_visualization/streamlit/divorce/rings.png
+", 
          use_container_width=True)
 
 st.markdown("""
