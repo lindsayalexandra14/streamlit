ca74eb6d5eb504f2c55903a6ef8a59051cb0ce5e
lindsayalexandra14 <149544151+lindsayalexandra14@users.noreply.github.com>
2025-11-14 19:49:37 +0000

    test

--- streamlit-crash-course/streamlit_cc_app.py
+++ streamlit-crash-course/streamlit_cc_app.py
@@ -91,7 +91,7 @@ custom_palette = [
 ]
 
 st.image("https://raw.githubusercontent.com/lindsayalexandra14/ds_portfolio/main/1_projects/data_visualization/streamlit/divorce/bridegroom.png",
-         width="150px")
+         width=150)
 
 st.markdown("""
 <style>
