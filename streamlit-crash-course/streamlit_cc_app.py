9521c1e72ab75d5f4c04a65222715bce8295a71e
lindsayalexandra14 <149544151+lindsayalexandra14@users.noreply.github.com>
2025-11-14 18:53:28 +0000

    test

--- streamlit-crash-course/streamlit_cc_app.py
+++ streamlit-crash-course/streamlit_cc_app.py
@@ -95,7 +95,7 @@ st.markdown("""
 /* Existing small caption style */
 .tight_caption {
     padding: 8px 12px;
-    background: #D6CCC7;  
+    background: #DBD4D0;  
     border: 1px solid #aaa; 
     border-radius: 12px; 
     font-size: 0.9rem;
