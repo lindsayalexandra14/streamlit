c4f601e03b3227196fb75168542d88453ed493a3
lindsayalexandra14 <149544151+lindsayalexandra14@users.noreply.github.com>
2025-11-14 01:41:50 +0000

    test

--- streamlit-crash-course/streamlit_cc_app.py
+++ streamlit-crash-course/streamlit_cc_app.py
@@ -225,7 +225,7 @@ def make_pairplot(
         bgcolor="rgba(211, 211, 211, 0.5)",
         bordercolor="gray",
         borderpad=6,
-        borderwidth=1, boxstyle='round,pad=0.5'
+        borderwidth=1
     )
 
     fig.add_annotation(
@@ -310,7 +310,7 @@ def make_correlation_heatmap(
         wrap=True,
         horizontalalignment='center',
         bbox=dict(facecolor='lightgray', alpha=0.5,
-                edgecolor='gray',boxstyle='round,pad=0.5')
+                edgecolor='gray')
     )
 
     return fig
@@ -426,7 +426,7 @@ def make_chi2_plot(
         wrap=True,
         horizontalalignment='center',
         bbox=dict(facecolor='lightgray', alpha=0.5,
-                edgecolor='gray', boxstyle='round,pad=0.5')
+                edgecolor='gray')
     )
 
     # Final touches
